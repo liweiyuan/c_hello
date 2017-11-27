831dd1f5ce094c900564a8cf25652604c41c9da8
liweiyuan <535767595@qq.com>
2017-11-27 21:32:31 +0800

    c语言练习指针与多维数组

---
+++ pointer/zippo1.c
@@ -0,0 +1,20 @@
+//
+// Created by spark on 17-11-27.
+//
+// 指针与多维数组的关系
+#include <stdio.h>
+int main(void){
+    int zippo[4][2]={{2,4},{6,8},{1,3},{5,7}};
+
+    printf("   zippo =  %p,  zippo +1 = %p\n",zippo,zippo+1);
+    printf("zippo[0] =  %p,  zippo[0] +1 = %p\n",zippo[0],zippo[0]+1);
+    printf(" *zippo =  %p,    *zippo +1 = %p\n",*zippo,*zippo+1);
+    printf("zippo[0][0] =  %d,  zippo[0][0] +1 = %d\n",zippo[0][0],zippo[0][0]+1);
+    printf(" *zippo[0] = %d\n",*zippo[0]);
+    printf("**zippo =%d\n",**zippo);
+    printf("  zippo[2][1] = %d\n",zippo[2][1]);
+    printf("  *(*(zippo+2)+1) = %d\n",*(*(zippo+2)+1));
+    
+    return 0;
+}
+
