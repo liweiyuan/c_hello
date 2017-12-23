da92c6a8c5525f9764ba1fdca133db0ea6be79ba
liweiyuan <535767595@qq.com>
2017-12-23 22:25:37 +0800

    c语言练习c语言与汇编转换理解

--- computer/exchange.c
+++ computer/exchange.c
@@ -1,9 +1,16 @@
 //
 // Created by spark on 17-12-23.
 //
+#include <stdio.h>
 long exchange(long *xp,long y){
     long x=*xp;
     *xp=y;
     return x;
+}
+int main(){
+    long a=4;
+    long b=exchange(&a,3);
+    printf("a = %ld, b = %ld",a,b);
+    return 0;
 }
 
---
+++ computer/leaq.c
@@ -0,0 +1,8 @@
+//
+// Created by spark on 17-12-23.
+//
+long scale(long x,long y,long z){
+    long t=x+4*y+12*z;
+    return t;
+}
+
